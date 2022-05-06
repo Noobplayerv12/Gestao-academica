0eb291aaa6ad3daaba43273c4cd418bd253e8fc1
Maxlipeman <104946979+Maxlipeman@users.noreply.github.com>
2022-05-05 22:01:50 -0300

    Update Setor_Universidade.java

--- entities/Setor_Universidade.java
+++ entities/Setor_Universidade.java
@@ -3,4 +3,28 @@ package entities;
 
 public class Setor_Universidade {
 
+    private int idsetor;
+    private String nomesetor;
+
+    public Setor_Universidade(int idsetor, String nomesetor) {
+        this.idsetor = idsetor;
+        this.nomesetor = nomesetor;                                                
+    }
+
+    public int getIdsetor() {
+        return idsetor;
+    }
+
+    public void setIdsetor(int idsetor) {
+        this.idsetor = idsetor;
+    }
+
+    public String informaNomesetor() {
+        return nomesetor;
+    }
+
+    public void setNomesetor(String nomesetor) {
+        this.nomesetor = nomesetor;
+    }
+   
 }
