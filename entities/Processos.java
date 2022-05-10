dc4474a903d0c74267af684dcd54ecd7101c7d23
Thiago da Silva Santos <50751401+thiagosiIva@users.noreply.github.com>
2022-05-10 19:21:15 -0300

    Classe processos/faltando gerar gettrs and settrs

---
+++ entities/Processos.java
@@ -0,0 +1,15 @@
+package entities;
+import java.util.Date;
+public class Processos{
+    private int numero;
+    private String finalidade;
+    private String descricao;
+    private Date dataEntrada;
+    private ArrayList<Setor_Universidade> Setor_Universidade;
+
+    
+
+
+
+
+}
