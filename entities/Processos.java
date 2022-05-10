1a19803271526d54d937635d61ceeed73ccc7cf4
Thiago da Silva Santos <50751401+thiagosiIva@users.noreply.github.com>
2022-05-10 20:34:51 -0300

    Correção classe processos

--- entities/Processos.java
+++ entities/Processos.java
@@ -10,16 +10,41 @@ public class Processos{
     private Funcionario funcionario;
     private Setor_Universidade setor;
 
-    private ArrayList<Setor_Universidade> Setor_Universidade;
-
-        public Processos(int numero, String finalidade, String descricao, Date dataEntrada) {
+     public Processos(int numero, String finalidade, String descricao, Date dataEntrada, Alunos aluno, Funcionario funcionario, Setor_Universidade setor) {
         this.numero = numero;
         this.finalidade = finalidade;
         this.descricao = descricao;
         this.dataEntrada = dataEntrada;
+        this.aluno = aluno;
+        this.funcionario = funcionario;
+        this.setor = setor;
     }
 
- public int getNumero() {
+    public Alunos getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Alunos aluno) {
+        this.aluno = aluno;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Setor_Universidade getSetor() {
+        return setor;
+    }
+
+    public void setSetor(Setor_Universidade setor) {
+        this.setor = setor;
+    }
+
+    public int getNumero() {
         return numero;
     }
 
