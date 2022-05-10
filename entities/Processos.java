bda75e0e0efc8d4ede26254a10ead865db063dc4
Thiago da Silva Santos <50751401+thiagosiIva@users.noreply.github.com>
2022-05-10 19:43:09 -0300

    Gettrs and settrs

--- entities/Processos.java
+++ entities/Processos.java
@@ -7,6 +7,44 @@ public class Processos{
     private Date dataEntrada;
     private ArrayList<Setor_Universidade> Setor_Universidade;
 
+        public Processos(int numero, String finalidade, String descricao, Date dataEntrada) {
+        this.numero = numero;
+        this.finalidade = finalidade;
+        this.descricao = descricao;
+        this.dataEntrada = dataEntrada;
+    }
+
+ public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFinalidade() {
+        return finalidade;
+    }
+
+    public void setFinalidade(String finalidade) {
+        this.finalidade = finalidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(Date dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
     
 
 
